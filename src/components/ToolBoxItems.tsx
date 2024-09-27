afeeec210f9b30d130455a32f8df28109a2afd2b
geekfreak123 <sonawanerohan502@gmail.com>
2024-09-27 21:45:40 +0530

    Call To Action Button Added

--- src/components/ToolBoxItems.tsx
+++ src/components/ToolBoxItems.tsx
@@ -1,24 +1,37 @@
+import { twMerge } from "tailwind-merge";
 import { TeachIcon } from "./TeachIcon";
 
 export const ToolBoxItems = ({
   items,
+  className,
+  itemsWrapperClassName,
 }: {
   items: {
     title: string;
     iconTypes: React.ElementType;
-  }[]
+  }[];
+  className ?: string;
+  itemsWrapperClassName ?: string;
 }) => {
   return (
-    <div>
-      {items.map((item) => (
-        <div
-          key={item.title}
-          className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg"
-        >
-          <TeachIcon component={item.iconType} />
-          <span className="font-semibold">{item.title}</span>
-        </div>
-      ))}
+    <div
+      className={twMerge(
+        "flex [mask-image: linear-gradient(to_r,transparent,black_10%,balc_90%,transparent)]",
+        className,
+        
+      )}
+    >
+      <div className={twMerge("flex flex-none py-0.5 gap-6 pr-6",itemsWrapperClassName,)}>
+        {items.map((item) => (
+          <div
+            key={item.title}
+            className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg"
+          >
+            <TeachIcon component={item.iconType} />
+            <span className="font-semibold">{item.title}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -46,79 +46,118 @@ const hobbies = [
   {
     title: "Reading",
     emoji: "📚",
+    left: "5%",
+    top: "5%",
   },
   {
     title: "Fitness",
     emoji: "⛹️‍♂️",
-  },
-  {
-    title: "Photography",
-    emoji: "📸",
+    left: "50%",
+    top: "5%",
   },
   {
     title: "Hiking",
     emoji: "🥾",
+    left: "35%",
+    top: "40%",
+  },
+  {
+    title: "Chess",
+    emoji: "♟️",
+    left: "10%",
+    top: "35%",
   },
   {
     title: "Singing",
     emoji: "🎤",
+    left: "70%",
+    top: "45%",
   },
   {
     title: "Painting",
     emoji: "🎨",
+    left: "5%",
+    top: "65%",
+  },
+  {
+    title: "Photography",
+    emoji: "📸",
+    left: "45%",
+    top: "70%",
   },
 ];
 
 export const AboutSection = () => {
   return (
-    <section className="py-20">
+    <section className="py-20 lg:py-28">
       <div className="container">
         <SectionHeader
           eyebrow="About Me"
           title="A Glimse Into My World"
           description="Learn more about who I am,what I do & what Inspires me."
         />
-        <div className="mt-20">
-          <Card className="h-[320px]">
-            <CardHeader
-              title="My Reads"
-              description="Explore the books shaping my perspective"
-            />
-            <div>
-              <Image
-                src={bookImage}
-                alt="Book Cover"
-                className="w-40 mx-auto mt-8 "
+        <div className="mt-20 flex flex-col gap-8">
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
+            <Card className="h-[320px] md:col-span-2 lg:col-span-1">
+              <CardHeader
+                title="My Reads"
+                description="Explore the books shaping my perspective"
               />
-            </div>
-          </Card>
-          <Card>
-            <CardHeader
-              title="My Toolbox"
-              description="Explore the technology I use to craft exceptional digital
+              <div className="w-40 mx-auto mt-2 md:mt-0">
+                <Image src={bookImage} alt="Book Cover" />
+              </div>
+            </Card>
+            <Card className="h-[320px]  md:col-span-3 lg:col-span-2">
+              <CardHeader
+                title="My Toolbox"
+                description="Explore the technology I use to craft exceptional digital
               experiences."
-            />
-          <ToolBoxItems items={toolboxItems} />
-          <ToolBoxItems items={toolboxItems} />
-          </Card>
-          <Card>
-            <CardHeader
-              title="Beyond the code"
-              description="Explore the interest & hobbies beyond the digital realm."
-            />
-            <div>
-              {hobbies.map((hobby) => (
-                <div key={hobby.title}>
-                  <span>{hobby.title}</span>
-                  <span>{hobby.emoji}</span>
-                </div>
-              ))}
-            </div>
-          </Card>
-          <Card>
-            <Image src={mapImage} alt="map" />
-            <Image src={smileEmoji} alt="Smile Emoji" />
-          </Card>
+                className=""
+              />
+              <ToolBoxItems items={toolboxItems} className="" />
+              <ToolBoxItems
+                items={toolboxItems}
+                className="mt-6"
+                itemsWrapperClassName="-translate-x-1/2"
+              />
+            </Card>
+          </div>
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
+            <Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
+              <CardHeader
+                title="Beyond the code"
+                description="Explore the interest & hobbies beyond the digital realm."
+                className="px-6 py-6"
+              />
+              <div className="relative flex-1">
+                {hobbies.map((hobby) => (
+                  <div
+                    key={hobby.title}
+                    className="inline-flex gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
+                    style={{
+                      left: hobby.left,
+                      top: hobby.top,
+                    }}
+                  >
+                    <span className="font-medium text-gray-950">
+                      {hobby.title}
+                    </span>
+                    <span>{hobby.emoji}</span>
+                  </div>
+                ))}
+              </div>
+            </Card>
+            <Card className="h-[320px] p-0 relative md:col-span-2 lg:col-span-1">
+              <Image
+                src={mapImage}
+                alt="map"
+                className="h-full w-full object-cover obj-left-top"
+              />
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-10 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-2 after:rounded-full after:gray-950/30">
+                <Image src={smileEmoji} alt="Smile Emoji" className="size-10" />
+              </div>
+            </Card>
+          </div>
         </div>
       </div>
     </section>
